d62c1668df09c926aa9a13676208f800859ed610
이청호 <79739183+CheongHo-Lee@users.noreply.github.com>
2021-06-23 15:33:31 +0900

    캐릭터이동 스크립트 완성 + 조사 인터랙션

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -9,10 +9,14 @@ public class PlayerAction : MonoBehaviour
     float v;
     public float speed;
     bool isHorizonMove;
+    Vector3 dirVec;
+    GameObject scanObject;
 
+    Rigidbody2D rigid;
 
     private void Awake()
     {
+        rigid = GetComponent<Rigidbody2D>();
     }
     // Start is called before the first frame update
     void Start()
@@ -24,25 +28,58 @@ public class PlayerAction : MonoBehaviour
     void Update()
     {
         //movw Value
-        h =  Input.GetAxisRaw("Horizontal");
+        h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
 
         //Check Button Bown & Up
-        bool hDown = Input.GetButton("Horizontal");
-        bool vDown = Input.GetButton("Vertical");
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
 
         bool hUp = Input.GetButtonUp("Horizontal");
         bool vUp = Input.GetButtonUp("Vertical");
 
         //Check Horizontal Move
-        if (hDown || vUp)
+        if (hDown)
             isHorizonMove = true;
-        else if (vDown || hUp)
+        else if (vDown)
             isHorizonMove = false;
+        else if (hUp || vUp)
+            isHorizonMove = h != 0;
 
-       
+        //Direction
+        if (vDown && v == 1)
+            dirVec = Vector3.up;
+        else if (vDown && v == -1)
+            dirVec = Vector3.down;
+        else if (hDown && h == -1)
+            dirVec = Vector3.left;
+        else if (hDown && h == 1)
+            dirVec = Vector3.right;
+
+        ////Move
+        //Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        //transform.Translate(moveVec * speed * Time.deltaTime);
+
+        //Ray
+        Debug.DrawRay(transform.position, dirVec * 0.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+
+        if (rayHit.collider != null)
+        {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else
+            scanObject = null;
+
+        //Scan Object
+        if (Input.GetButtonDown("Jump") && scanObject != null)
+            Debug.Log("scan : " + scanObject);
+    }
+
+    private void FixedUpdate()
+    {
         //Move
         Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
-        transform.Translate(moveVec * speed * Time.deltaTime);
+        rigid.velocity = moveVec * speed;
     }
 }
