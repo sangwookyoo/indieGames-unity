0b9ede4f566a05397610240f89fe849b9d446370
이청호 <79739183+CheongHo-Lee@users.noreply.github.com>
2021-06-24 17:29:02 +0900

    대화창
    엑셀에서 데이터 받아와서 대화창에 읽기

---
+++ Assets/02.Scripts/TalkManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TalkManager : MonoBehaviour
+{
+    Dictionary<int, string[]> talkData; //배열로 스트링한 이유는 한 대화에 여러 대화가 있을 수 있기 때문
+
+    void Awake()
+    {
+        talkData = new Dictionary<int, string[]>();
+        GenerateData();
+    }
+        // Update is called once per frame
+    void GenerateData()
+    {
+        TextAsset sr = Resources.Load("TalkFileTxtTest") as TextAsset;
+        StringReader reader = new StringReader(sr.text);
+
+        bool endOfFile = false;
+        while (!endOfFile)
+        {
+            string data_String = reader.ReadLine();
+            if (data_String == null)
+            {
+                endOfFile = true;
+                break;
+            }
+            var data_values = data_String.Split(',');
+
+            
+            talkData.Add(int.Parse(data_values[0]), new string[] { data_values[1], data_values[2]});
+            
+        }
+    }
+
+    public string GetTalk(int id, int talkIndex)
+    {
+        if (talkIndex == talkData[id].Length)
+            return null;
+        else
+            return talkData[id][talkIndex];
+    }
+}
