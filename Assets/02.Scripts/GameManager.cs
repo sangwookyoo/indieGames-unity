600f39407cd4307d96036d056f975b832f75f1e4
이청호 <uirobol@gmail.com>
2021-07-04 20:16:41 +0900

    저장하기1차수정

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -8,18 +8,25 @@ public class GameManager : MonoBehaviour
     public TalkManager talkManager;
     public Text talkText;
     public bool isAction;
+    public bool isLoadFile1;
+    public bool isLoadFile2;
+    public bool isLoadFile3;
     public int talkIndex;
     public GameObject MenuSet;
     public GameObject scanObject;
     public GameObject talkPanel;
     public GameObject Player;
     // Start is called before the first frame update
+    
 
 
     private void Start()
     {
+        Debug.Log(PlayerPrefs.GetInt("isLoadFile"));
         GameLoad();
     }
+
+   
     private void Update()
     {
         if(Input.GetButtonDown("Cancel"))
@@ -68,19 +75,62 @@ public class GameManager : MonoBehaviour
         Application.Quit();
     }
 
-    public void GameSave()
+    #region 저장하기
+    public void GameSave1()
     {
-        PlayerPrefs.SetFloat("PlayerX" ,Player.transform.position.x);
-        PlayerPrefs.SetFloat("PlayerY" ,Player.transform.position.y);
+        PlayerPrefs.SetFloat("FirstPlayerX" ,Player.transform.position.x);
+        PlayerPrefs.SetFloat("FirstPlayerY" ,Player.transform.position.y);
         PlayerPrefs.Save();
-
         MenuSet.SetActive(false);
     }
+    public void GameSave2()
+    {
+        PlayerPrefs.SetFloat("SecondPlayerX", Player.transform.position.x);
+        PlayerPrefs.SetFloat("SecondPlayerY", Player.transform.position.y);
+        PlayerPrefs.Save();
+        MenuSet.SetActive(false);
+    }
+    public void GameSave3()
+    {
+        PlayerPrefs.SetFloat("ThirdPlayerX", Player.transform.position.x);
+        PlayerPrefs.SetFloat("ThirdPlayerY", Player.transform.position.y);
+        PlayerPrefs.Save();
+        MenuSet.SetActive(false);
+    }
+    #endregion
+    #region 불러오기
     public void GameLoad()
     {
-        float x = PlayerPrefs.GetFloat("PlayerX");
-        float y = PlayerPrefs.GetFloat("PlayerY");
+        if (PlayerPrefs.HasKey("FirstPlayerX") || PlayerPrefs.HasKey("SecondPlayerX") || PlayerPrefs.HasKey("ThirdPlayerX"))
+        {
+            if (PlayerPrefs.GetInt("isLoadFile") == 1)
+            {
+                float x = PlayerPrefs.GetFloat("FirstPlayerX");
+                float y = PlayerPrefs.GetFloat("FirstPlayerY");
 
-        Player.transform.position = new Vector3(x, y, 0);
+                Player.transform.position = new Vector2(x, y);
+            }
+            else if (PlayerPrefs.GetInt("isLoadFile") == 2)
+            {
+                float x = PlayerPrefs.GetFloat("SecondPlayerX");
+                float y = PlayerPrefs.GetFloat("SecondPlayerY");
+
+                Player.transform.position = new Vector2(x, y);
+            }
+            else if (PlayerPrefs.GetInt("isLoadFile") == 3)
+            {
+                float x = PlayerPrefs.GetFloat("ThirdPlayerX");
+                float y = PlayerPrefs.GetFloat("ThirdPlayerY");
+
+                Player.transform.position = new Vector2(x, y);
+            }
+            else if (PlayerPrefs.GetInt("isLoadFile") == 0)
+            {
+                Player.transform.position = new Vector2(1, 1);
+            }
+        }
     }
+  
+    #endregion
 }
+s
--- Assets/02.Scripts/SceneManager/LoadManager.cs
+++ Assets/02.Scripts/SceneManager/LoadManager.cs
@@ -6,10 +6,34 @@ using UnityEngine.SceneManagement;
 public class LoadManager : MonoBehaviour
 {
     public string sceneName;
+    public bool isLoadFile1 = false;
+    public bool isLoadFile2 = false;
+    public bool isLoadFile3 = false;
+    public GameObject Player;
 
     public void OnButtonClick()
     {
+        PlayerPrefs.SetInt("isLoadFile", 0);
         SceneManager.LoadSceneAsync(sceneName);
         Debug.Log(sceneName + "로 이동합니다.");
     }
+    public void LoadFile1Click()
+    {
+        PlayerPrefs.SetInt("isLoadFile", 1);
+        SceneManager.LoadSceneAsync(sceneName);
+        Debug.Log(sceneName + "로 이동합니다.");
+    }
+    public void LoadFile2Click()
+    {
+        PlayerPrefs.SetInt("isLoadFile", 2);
+        SceneManager.LoadSceneAsync(sceneName);
+        Debug.Log(sceneName + "로 이동합니다.");
+    }
+    public void LoadFile3Click()
+    {
+        PlayerPrefs.SetInt("isLoadFile", 3);
+        SceneManager.LoadSceneAsync(sceneName);
+        Debug.Log(sceneName + "로 이동합니다.");
+    }
+
 }
