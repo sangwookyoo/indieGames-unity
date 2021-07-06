9bf7d07e4a70ee403a2eb06dfdf3ad98d95b5a44
유상욱 <tkddnr032@gmail.com>
2021-07-07 03:37:37 +0900

    옥상 변경

---
+++ Assets/02.Scripts/Roof.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Roof : MonoBehaviour
+{
+    public AudioClip EffectSound;
+    public AudioSource audioSource;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Roof")
+        {
+            audioSource.PlayOneShot(EffectSound);
+        }
+    }
+}
