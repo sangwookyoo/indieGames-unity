28172478da13ab512fec9ebc79d13c897444a49e
이청호 <uirobol@gmail.com>
2021-07-06 14:34:15 +0900

    일기장인터랙션

---
+++ Assets/02.Scripts/QuestManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public int questId;
+    Dictionary<int, QuestData> questList;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        questList = new Dictionary<int, QuestData>();
+        GenerateData();
+    }
+
+    // Update is called once per frame
+   void GenerateData()
+    {
+        questList.Add(10, new QuestData("일기장을 찾아라", new int[] { }));
+    }
+}
